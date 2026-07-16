c86fcb3d091641eb32f4805cc0c427ace3e5feed
Dana Shalev <danashalev100@gmail.com>
2026-07-16 07:03:53 +0700

    Show Details

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,14 @@
 const DETAILS_SHEET_ID = 2109063306;
 
-const paramMap = {
-  "construction expenses": "Construction Expenses",
-  "infrastructure expenses": "Infrastructure Expenses",
-  "operational expenses": "Operational Expenses",
-  "land expenses": "Land Expenses",
-};
+const params = [
+  "construction expenses",
+  "setup expenses",
+  "operational expenses",
+  "land expenses"
+];
 
 function showDetails(param, month) {
-  const detailsParam = paramMap[param];
-
-  if (!detailsParam) {
+  if (!params.find((value) => param === value)) {
     SpreadsheetApp.getUi().alert(
       `"${param}" does not have a details view.`
     );
@@ -19,7 +17,7 @@ function showDetails(param, month) {
 
   const detailsSheet = SpreadsheetApp.getActive().getSheetById(DETAILS_SHEET_ID);
 
-  detailsSheet.getRange("B1").setValue(detailsParam);
+  detailsSheet.getRange("B1").setValue(SOLLibrary.capitalize(param));
   detailsSheet.getRange("B2").setValue(month);
 
   detailsSheet.activate();
